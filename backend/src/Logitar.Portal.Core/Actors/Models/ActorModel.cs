aa24e67ec29371d60832eb30e20d2caf21668264
Francis Pion <francispion@hotmail.com>
2022-10-27 22:59:34 -0400

    Fixed model setters.

--- backend/src/Logitar.Portal.Core/Actors/Models/ActorModel.cs
+++ backend/src/Logitar.Portal.Core/Actors/Models/ActorModel.cs
@@ -2,13 +2,13 @@
 {
   public class ActorModel
   {
-    public Guid Id { get; private set; }
+    public Guid Id { get; set; }
 
-    public ActorType Type { get; private set; }
-    public string Name { get; private set; } = null!;
-    public bool IsDeleted { get; private set; }
+    public ActorType Type { get; set; }
+    public string Name { get; set; } = null!;
+    public bool IsDeleted { get; set; }
 
-    public string? Email { get; private set; }
-    public string? Picture { get; private set; }
+    public string? Email { get; set; }
+    public string? Picture { get; set; }
   }
 }
