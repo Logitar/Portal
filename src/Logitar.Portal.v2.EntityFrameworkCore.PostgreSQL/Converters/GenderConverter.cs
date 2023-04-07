7bbe200d31af14ac3e0d782db8e6d25b315f6b8d
Francis Pion <francispion@hotmail.com>
2023-04-07 12:06:00 -0400

    Added JSON converters.

---
+++ src/Logitar.Portal.v2.EntityFrameworkCore.PostgreSQL/Converters/GenderConverter.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.EventSourcing.EntityFrameworkCore.PostgreSQL;
+using Logitar.Portal.v2.Core.Users;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Logitar.Portal.v2.EntityFrameworkCore.PostgreSQL.Converters;
+
+internal class GenderConverter : JsonConverter<Gender>
+{
+  static GenderConverter()
+  {
+    EventSerializer.Instance.RegisterConverter(new GenderConverter());
+  }
+
+  public override Gender Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    string? value = reader.GetString();
+
+    return value == null ? default : new Gender(value);
+  }
+
+  public override void Write(Utf8JsonWriter writer, Gender value, JsonSerializerOptions options)
+  {
+    writer.WriteStringValue(value.Value);
+  }
+}
---
+++ src/Logitar.Portal.v2.EntityFrameworkCore.PostgreSQL/Converters/TimeZoneEntryConverter.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.EventSourcing.EntityFrameworkCore.PostgreSQL;
+using Logitar.Portal.v2.Core.Users;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Logitar.Portal.v2.EntityFrameworkCore.PostgreSQL.Converters;
+
+internal class TimeZoneEntryConverter : JsonConverter<TimeZoneEntry>
+{
+  static TimeZoneEntryConverter()
+  {
+    EventSerializer.Instance.RegisterConverter(new TimeZoneEntryConverter());
+  }
+
+  public override TimeZoneEntry Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    string? id = reader.GetString();
+
+    return id == null ? default : new TimeZoneEntry(id);
+  }
+
+  public override void Write(Utf8JsonWriter writer, TimeZoneEntry value, JsonSerializerOptions options)
+  {
+    writer.WriteStringValue(value.Id);
+  }
+}
