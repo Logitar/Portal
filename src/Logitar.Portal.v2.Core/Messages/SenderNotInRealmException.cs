6adf83eb00a9a0d9f835e35e8f4d6c5e885599a7
Francis Pion <francis.pion@akinox.com>
2023-04-11 17:13:26 -0400

    Added exceptions & handlers.

---
+++ src/Logitar.Portal.v2.Core/Messages/SenderNotInRealmException.cs
@@ -0,0 +1,28 @@
+﻿using Logitar.Portal.v2.Core.Realms;
+using Logitar.Portal.v2.Core.Senders;
+using System.Text;
+
+namespace Logitar.Portal.v2.Core.Messages;
+
+public class SenderNotInRealmException : Exception
+{
+  public SenderNotInRealmException(SenderAggregate sender, RealmAggregate realm, string paramName)
+    : base(GetMessage(sender, realm, paramName))
+  {
+    Data["Sender"] = sender.ToString();
+    Data["Realm"] = realm.ToString();
+    Data["ParamName"] = paramName;
+  }
+
+  private static string GetMessage(SenderAggregate sender, RealmAggregate realm, string paramName)
+  {
+    StringBuilder message = new();
+
+    message.AppendLine("The specified sender does not belong to the specified realm.");
+    message.Append("Sender: ").Append(sender).AppendLine();
+    message.Append("Realm: ").Append(realm).AppendLine();
+    message.Append("ParamName: ").AppendLine(paramName);
+
+    return message.ToString();
+  }
+}
