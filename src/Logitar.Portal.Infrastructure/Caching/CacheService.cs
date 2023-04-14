c484c7ec9140bee243b5a3b8a4cdf033cbb765f5
Francis Pion <francispion@hotmail.com>
2023-04-14 01:05:30 -0400

    Clear sessions when clearing user.

--- src/Logitar.Portal.Infrastructure/Caching/CacheService.cs
+++ src/Logitar.Portal.Infrastructure/Caching/CacheService.cs
@@ -31,27 +31,71 @@ internal class CacheService : ICacheService
   private static string GetActorCacheKey(Guid id) => string.Join(':', nameof(Actor), id);
 
   public Session? GetSession(Guid id) => GetItem<Session>(GetSessionCacheKey(id));
-  public void RemoveSession(SessionAggregate session) => RemoveItem(GetSessionCacheKey(session.Id.ToGuid()));
-  public void SetSession(Session session) => SetItem(GetSessionCacheKey(session.Id), session, TimeSpan.FromMinutes(1));
+  public void RemoveSession(SessionAggregate session)
+  {
+    string sessionKey = GetSessionCacheKey(session.Id.ToGuid());
+    RemoveItem(sessionKey);
+
+    string userSessionsKey = GetUserSessionsCacheKey(session.UserId.ToGuid());
+    HashSet<string>? sessions = GetItem<HashSet<string>>(userSessionsKey);
+    if (sessions != null)
+    {
+      sessions.Remove(sessionKey);
+
+      if (sessions.Any())
+      {
+        SetItem(userSessionsKey, session, TimeSpan.FromMinutes(1));
+      }
+      else
+      {
+        RemoveItem(userSessionsKey);
+      }
+    }
+  }
+  public void SetSession(Session session)
+  {
+    TimeSpan expires = TimeSpan.FromMinutes(1);
+
+    string sessionKey = GetSessionCacheKey(session.Id);
+    SetItem(sessionKey, session, expires);
+
+    string userSessionsKey = GetUserSessionsCacheKey(session.User.Id);
+    HashSet<string> sessions = GetItem<HashSet<string>>(userSessionsKey) ?? new();
+    sessions.Add(sessionKey);
+    SetItem(userSessionsKey, sessions, expires);
+  }
   private static string GetSessionCacheKey(Guid id) => string.Join(':', "Session", id);
+  private static string GetUserSessionsCacheKey(Guid userId) => string.Join(':', "User", userId, "Sessions");
 
   public CachedUser? GetUser(string username) => GetItem<CachedUser>(GetUserCacheKey(username));
   public void RemoveUser(UserAggregate user)
   {
-    string key = GetUsernameCacheKey(user);
-    string? usernameKey = GetItem<string>(key);
-    if (usernameKey != null)
+    string usernameKey = GetUsernameCacheKey(user);
+    string? userKey = GetItem<string>(usernameKey);
+    if (userKey != null)
     {
-      RemoveItem(key);
       RemoveItem(usernameKey);
+      RemoveItem(userKey);
+    }
+
+    string userSessionsKey = GetUserSessionsCacheKey(user.Id.ToGuid());
+    HashSet<string>? sessions = GetItem<HashSet<string>>(userSessionsKey);
+    if (sessions != null)
+    {
+      foreach (string sessionKey in sessions)
+      {
+        RemoveItem(sessionKey);
+      }
+
+      RemoveItem(userSessionsKey);
     }
   }
   public void SetUser(string username, CachedUser user)
   {
     TimeSpan expires = TimeSpan.FromMinutes(1);
-    object key = GetUserCacheKey(username);
-    SetItem(key, user, expires);
-    SetItem(GetUsernameCacheKey(user.Aggregate), key, expires);
+    object userKey = GetUserCacheKey(username);
+    SetItem(userKey, user, expires);
+    SetItem(GetUsernameCacheKey(user.Aggregate), userKey, expires);
   }
   private static string GetUserCacheKey(string username) => string.Join(':', "User", "Username", username.Trim().ToUpper());
   private static string GetUsernameCacheKey(UserAggregate user) => string.Join(':', "Username", user.Id);
