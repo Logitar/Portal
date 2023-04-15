90568b92329549ca22bf2a649fc66fa59048c445
Francis Pion <francispion@hotmail.com>
2023-04-15 00:39:59 -0400

    Implemented client & endpoint tests.

---
+++ tests/Logitar.Portal.Client.IntegrationTests/Program.cs
@@ -0,0 +1,110 @@
+﻿using Logitar.Portal.Contracts.Realms;
+using Logitar.Portal.Contracts.Users;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Portal.Client;
+
+internal class Program
+{
+  static async Task Main()
+  {
+    IConfiguration configuration = new ConfigurationBuilder()
+      .AddUserSecrets("8d2d2156-5c9e-4d5d-828e-64f3c37d5829")
+      .Build();
+
+    PortalSettings settings = new()
+    {
+      BaseUrl = configuration.GetValue<string>("BaseUrl") ?? string.Empty
+    };
+    Credentials credentials = configuration.GetSection("Credentials").Get<Credentials>() ?? new();
+
+    using HttpClient client = new();
+    ConfigurationService configurationService = new(client, settings);
+    TestContext context = new(count: 48);
+    context.Start();
+
+    ConfigurationServiceTests configurationTests = new(configurationService);
+    await configurationTests.ExecuteAsync(context, credentials); // 1 test
+
+    if (context.HasFailed)
+    {
+      context.End();
+      return;
+    }
+
+    settings.BasicAuthentication = credentials;
+
+    IServiceProvider serviceProvider = new ServiceCollection()
+      .AddHttpClient()
+      .AddLogitarPortalClient(settings)
+      .AddSingleton(configuration)
+      .AddSingleton(context)
+      .AddTransient<DictionaryServiceTests>()
+      .AddTransient<MessageServiceTests>()
+      .AddTransient<RealmServiceTests>()
+      .AddTransient<SenderServiceTests>()
+      .AddTransient<SessionServiceTests>()
+      .AddTransient<TemplateServiceTests>()
+      .AddTransient<TokenServiceTests>()
+      .AddTransient<UserServiceTests>()
+      .BuildServiceProvider();
+
+    RealmServiceTests realmTests = serviceProvider.GetRequiredService<RealmServiceTests>();
+    Realm? realm = await realmTests.ExecuteAsync(); // 5 tests
+    if (realm == null)
+    {
+      context.End();
+      return;
+    }
+    context.Realm = realm;
+
+    DictionaryServiceTests dictionaryTests = serviceProvider.GetRequiredService<DictionaryServiceTests>(); // 5 tests
+    if (await dictionaryTests.ExecuteAsync() == null)
+    {
+      context.End();
+      return;
+    }
+
+    SenderServiceTests senderTests = serviceProvider.GetRequiredService<SenderServiceTests>(); // 6 tests
+    if (await senderTests.ExecuteAsync() == null)
+    {
+      context.End();
+      return;
+    }
+
+    TemplateServiceTests templateTests = serviceProvider.GetRequiredService<TemplateServiceTests>(); // 5 tests
+    if (await templateTests.ExecuteAsync() == null)
+    {
+      context.End();
+      return;
+    }
+
+    TokenServiceTests tokenTests = serviceProvider.GetRequiredService<TokenServiceTests>(); // 3 tests
+    if (!await tokenTests.ExecuteAsync())
+    {
+      context.End();
+      return;
+    }
+
+    UserServiceTests userTests = serviceProvider.GetRequiredService<UserServiceTests>(); // 14 tests
+    User? user = await userTests.ExecuteAsync(credentials);
+    if (user == null)
+    {
+      context.End();
+      return;
+    }
+
+    SessionServiceTests sessionTests = serviceProvider.GetRequiredService<SessionServiceTests>(); // 6 tests
+    if (await sessionTests.ExecuteAsync(credentials) == null)
+    {
+      context.End();
+      return;
+    }
+
+    MessageServiceTests messageTests = serviceProvider.GetRequiredService<MessageServiceTests>(); // 6 tests
+    _ = await messageTests.ExecuteAsync(user);
+
+    context.End();
+  }
+}
---
+++ tests/Logitar.Portal.Client.IntegrationTests/RealmServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Logitar.Portal.Client.Implementations;
+using Logitar.Portal.Contracts.Realms;
+
+namespace Logitar.Portal.Client;
+
+internal class RealmServiceTests
+{
+  private readonly TestContext _context;
+  private readonly IRealmService _realmService;
+
+  public RealmServiceTests(TestContext context, IRealmService realmService)
+  {
+    _context = context;
+    _realmService = realmService;
+  }
+
+  public async Task<Realm?> ExecuteAsync(CancellationToken cancellationToken = default)
+  {
+    string name = string.Empty;
+    try
+    {
+      name = string.Join('.', nameof(RealmService), nameof(RealmService.CreateAsync));
+      CreateRealmInput input = new()
+      {
+        UniqueName = "test"
+      };
+      Realm realm = await _realmService.CreateAsync(input, cancellationToken);
+      _context.Succeed(name);
+
+      name = string.Join('.', nameof(RealmService), nameof(RealmService.UpdateAsync));
+      UpdateRealmInput update = new()
+      {
+        DisplayName = "Test Realm",
+        Description = "    ",
+        DefaultLocale = "en-CA",
+        Url = "https://www.test.com/",
+        RequireConfirmedAccount = true,
+        RequireUniqueEmail = true
+      };
+      realm = await _realmService.UpdateAsync(realm.Id, update, cancellationToken);
+      _context.Succeed(name);
+
+      name = string.Join('.', nameof(RealmService), nameof(RealmService.GetAsync));
+      realm = (await _realmService.GetAsync(search: null, cancellationToken: cancellationToken)).Items.Single();
+      _context.Succeed(name);
+
+      name = string.Join('.', nameof(RealmService), $"{nameof(RealmService.GetAsync)}(id)");
+      realm = await _realmService.GetAsync(realm.Id, cancellationToken: cancellationToken)
+        ?? throw new InvalidOperationException("The result cannot be null.");
+      _context.Succeed(name);
+
+      name = string.Join('.', nameof(RealmService), nameof(RealmService.DeleteAsync));
+      Guid deleteId = (await _realmService.CreateAsync(new CreateRealmInput { UniqueName = "delete-me" }, cancellationToken)).Id;
+      _ = await _realmService.DeleteAsync(deleteId, cancellationToken);
+      _context.Succeed(name);
+
+      return realm;
+    }
+    catch (Exception exception)
+    {
+      _context.Fail(name, exception);
+
+      return null;
+    }
+  }
+}
