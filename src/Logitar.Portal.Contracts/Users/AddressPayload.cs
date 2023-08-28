563435b5e62ff9e745edf1baf7f03e19f667f50b
Francis Pion <francispion@hotmail.com>
2023-08-28 18:51:52 -0400

    Added user contracts.

---
+++ src/Logitar.Portal.Contracts/Users/AddressPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace Logitar.Portal.Contracts.Users;
+
+public record AddressPayload
+{
+  public string Street { get; set; } = string.Empty;
+  public string Locality { get; set; } = string.Empty;
+  public string? Region { get; set; }
+  public string? PostalCode { get; set; }
+  public string Country { get; set; } = string.Empty;
+  public bool IsVerified { get; set; }
+}
