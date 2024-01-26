537b730087b25e9d2e985737503a7f4fd177bb1f
Francis Pion <francispion@hotmail.com>
2024-01-25 22:05:08 -0500

    Implemented Basic authentication and Open API Security.

---
+++ backend/src/Logitar.Portal/Constants/Headers.cs
@@ -0,0 +1,7 @@
+﻿namespace Logitar.Portal.Constants;
+
+internal static class Headers
+{
+  public const string Authorization = nameof(Authorization);
+  public const string ApiKey = "X-API-Key";
+}
