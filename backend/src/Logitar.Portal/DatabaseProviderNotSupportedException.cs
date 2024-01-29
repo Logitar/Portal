170fd9cc3a1af0c972ef28cec0be4b2249a51317
Francis Pion <francispion@hotmail.com>
2024-01-28 20:22:22 -0500

    Created a Web API.

---
+++ backend/src/Logitar.Portal/DatabaseProviderNotSupportedException.cs
@@ -0,0 +1,22 @@
+﻿namespace Logitar.Portal;
+
+internal class DatabaseProviderNotSupportedException : NotSupportedException
+{
+  public const string ErrorMessage = "The specified database provider is not supported.";
+
+  public DatabaseProvider DatabaseProvider
+  {
+    get => (DatabaseProvider)Data[nameof(DatabaseProvider)]!;
+    private set => Data[nameof(DatabaseProvider)] = value;
+  }
+
+  public DatabaseProviderNotSupportedException(DatabaseProvider databaseProvider)
+    : base(BuildMessage(databaseProvider))
+  {
+    DatabaseProvider = databaseProvider;
+  }
+
+  private static string BuildMessage(DatabaseProvider databaseProvider) => new ErrorMessageBuilder(ErrorMessage) // TODO(fpion): remove the Logitar NuGet package when possible.
+    .AddData(nameof(DatabaseProvider), databaseProvider)
+    .Build();
+}
