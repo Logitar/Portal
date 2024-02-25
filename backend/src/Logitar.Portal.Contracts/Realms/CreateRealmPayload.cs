efec0d3671bd3448710a4ca72699c71a176d0555
Francis Pion <francispion@hotmail.com>
2024-02-25 18:11:58 -0500

    Added realm types and Graph types.

--- backend/src/Logitar.Portal.Contracts/Realms/CreateRealmPayload.cs
+++ backend/src/Logitar.Portal.Contracts/Realms/CreateRealmPayload.cs
@@ -9,11 +9,11 @@ public record CreateRealmPayload
   public string? Description { get; set; }
 
   public string? DefaultLocale { get; set; }
-  public string Secret { get; set; }
+  public string Secret { get; set; } // TODO(fpion): nullable
   public string? Url { get; set; }
 
-  public UniqueNameSettings UniqueNameSettings { get; set; }
-  public PasswordSettings PasswordSettings { get; set; }
+  public UniqueNameSettings UniqueNameSettings { get; set; } // TODO(fpion): nullable
+  public PasswordSettings PasswordSettings { get; set; } // TODO(fpion): nullable
   public bool RequireUniqueEmail { get; set; }
 
   public List<CustomAttribute> CustomAttributes { get; set; }
---
+++ backend/src/Logitar.Portal.GraphQL/Realms/RealmGraphType.cs
@@ -0,0 +1,35 @@
+﻿using GraphQL.Types;
+using Logitar.Portal.Contracts.Realms;
+using Logitar.Portal.GraphQL.Settings;
+
+namespace Logitar.Portal.GraphQL.Realms;
+
+internal class RealmGraphType : AggregateGraphType<Realm>
+{
+  public RealmGraphType() : base("Represents a hard tenant into the identity system.")
+  {
+    Field(x => x.UniqueSlug)
+      .Description("The unique slug of the realm.");
+    Field(x => x.DisplayName, nullable: true)
+      .Description("The display name of the realm.");
+    Field(x => x.Description, nullable: true)
+      .Description("The description of the realm.");
+
+    Field(x => x.DefaultLocale, type: typeof(LocaleGraphType))
+      .Description("The default language of the realm.");
+    Field(x => x.Secret)
+      .Description("The secret to use to sign security tokens of the realm.");
+    Field(x => x.Url, nullable: true)
+      .Description("The primary URL of the application that is represented by the realm.");
+
+    Field(x => x.UniqueNameSettings, type: typeof(NonNullGraphType<UniqueNameSettingsGraphType>))
+      .Description("The settings of the unique names in the realm.");
+    Field(x => x.PasswordSettings, type: typeof(NonNullGraphType<PasswordSettingsGraphType>))
+      .Description("The settings of the passwords in the realm.");
+    Field(x => x.RequireUniqueEmail)
+      .Description("A value indicating whether or not email addresses are unique in the realm.");
+
+    Field(x => x.CustomAttributes, type: typeof(NonNullGraphType<ListGraphType<NonNullGraphType<CustomAttributeGraphType>>>))
+      .Description("The custom attributes of the realm.");
+  }
+}
