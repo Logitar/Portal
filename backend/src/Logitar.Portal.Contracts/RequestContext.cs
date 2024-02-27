0390826bec1ba8cbc4f0e67b812dacd34c52e1d4
Francis Pion <francispion@hotmail.com>
2024-02-27 13:40:22 -0500

    Implemented a RequestContext.

---
+++ backend/src/Logitar.Portal.Contracts/RequestContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Logitar.Portal.Contracts;
+
+public record RequestContext : IRequestContext
+{
+  public string? User { get; set; }
+
+  public CancellationToken CancellationToken { get; set; }
+
+  public RequestContext() : this(cancellationToken: default)
+  {
+  }
+
+  public RequestContext(CancellationToken cancellationToken) : this(user: null, cancellationToken)
+  {
+  }
+
+  public RequestContext(string? user, CancellationToken cancellationToken = default)
+  {
+    User = user;
+    CancellationToken = cancellationToken;
+  }
+}
