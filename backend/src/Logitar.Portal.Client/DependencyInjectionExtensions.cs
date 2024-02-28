6d94eae1467aecd1582eb336b5918af8c720c034
Francis Pion <francispion@hotmail.com>
2024-02-28 18:24:21 -0500

    Added DependencyInjectionExtensions.

---
+++ backend/src/Logitar.Portal.Client/DependencyInjectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Logitar.Portal.Client.Configurations;
+using Logitar.Portal.Contracts.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Portal.Client;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddLogitarPortalClient(this IServiceCollection services, IConfiguration configuration)
+  {
+    PortalSettings settings = configuration.GetSection("Portal").Get<PortalSettings>() ?? new();
+    return services.AddLogitarPortalClient(settings);
+  }
+  public static IServiceCollection AddLogitarPortalClient(this IServiceCollection services, IPortalSettings settings)
+  {
+    return services
+      .AddHttpClient()
+      .AddSingleton(settings)
+      .AddSingleton<IConfigurationClient, ConfigurationClient>();
+  }
+}
