c052f63327696b913632bcf8c0e19776d96040a3
Francis Pion <francispion@hotmail.com>
2024-02-28 19:54:29 -0500

    Implementing a ConsoleApp.

---
+++ backend/tests/Logitar.Portal.Client.E2ETests/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Portal.Client;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    IConfiguration configuration = new ConfigurationBuilder()
+      .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+      .AddUserSecrets("b2b773e6-c1b2-4500-bcfa-a92c3e545a9c")
+      .AddCommandLine(args)
+      .Build();
+
+    ServiceCollection services = new();
+    services.AddSingleton(configuration);
+    IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+    // TODO(fpion): implement
+  }
+}
