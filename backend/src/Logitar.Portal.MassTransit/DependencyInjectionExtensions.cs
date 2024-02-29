debc3f0d013ec91680f157ab20f7bcc36891db75
Francis Pion <francispion@hotmail.com>
2024-02-29 11:29:09 -0500

    Created a MassTransit interface.

---
+++ backend/src/Logitar.Portal.MassTransit/DependencyInjectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Logitar.Portal.MassTransit.Settings;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Logitar.Portal.MassTransit;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddLogitarPortalMassTransit(this IServiceCollection services, IConfiguration configuration)
+  {
+    MassTransitSettings settings = configuration.GetSection("MassTransit").Get<MassTransitSettings>() ?? new();
+    return services.AddLogitarPortalMassTransit(settings);
+  }
+  public static IServiceCollection AddLogitarPortalMassTransit(this IServiceCollection services, IMassTransitSettings settings)
+  {
+    if (settings.TransportBroker.HasValue)
+    {
+      services.AddMassTransit(configurator =>
+      {
+        switch (settings.TransportBroker.Value)
+        {
+          case TransportBroker.RabbitMQ:
+            if (settings.RabbitMQ == null)
+            {
+              throw new ArgumentException($"The {nameof(settings.RabbitMQ)} is required.", nameof(settings));
+            }
+            configurator.AddMassTransitRabbitMQ(settings.RabbitMQ);
+            break;
+          default:
+            throw new TransportBrokerNotSupportedException(settings.TransportBroker.Value);
+        }
+        configurator.AddConsumers(Assembly.GetExecutingAssembly().GetTypes().Where(type => typeof(IConsumer).IsAssignableFrom(type)).ToArray());
+      });
+    }
+
+    return services;
+  }
+
+  private static void AddMassTransitRabbitMQ(this IBusRegistrationConfigurator configurator, RabbitMqSettings settings)
+  {
+    configurator.UsingRabbitMq((context, config) =>
+    {
+      config.Host(settings.Host, settings.Port, settings.VirtualHost, h =>
+      {
+        h.Username(settings.Username);
+        h.Password(settings.Password);
+      });
+      config.ConfigureEndpoints(context);
+    });
+  }
+}
---
+++ backend/src/Logitar.Portal.MassTransit/Messages/SendMessageCommandConsumer.cs
@@ -0,0 +1,32 @@
+﻿using Logitar.Portal.Application.Messages.Commands;
+using Logitar.Portal.Contracts.Messages;
+using MassTransit;
+using MediatR;
+
+namespace Logitar.Portal.MassTransit.Messages;
+
+internal class SendMessageCommandConsumer : IConsumer<SendMessageCommand>
+{
+  private readonly IBus _bus;
+  private readonly IMediator _mediator;
+
+  public SendMessageCommandConsumer(IBus bus, IMediator mediator)
+  {
+    _bus = bus;
+    _mediator = mediator;
+  }
+
+  public async Task Consume(ConsumeContext<SendMessageCommand> context)
+  {
+    SendMessageCommand command = context.Message;
+    CancellationToken cancellationToken = context.CancellationToken;
+
+    SendMessageInternalCommand request = new(command.Payload);
+    //request.Populate(actor, configuration, realm); // TODO(fpion): populate request
+
+    SentMessages sentMessages = await _mediator.Send(request, cancellationToken);
+    MessagesSentEvent @event = new(sentMessages);
+
+    await _bus.Publish(@event, c => c.CorrelationId = context.CorrelationId, cancellationToken);
+  }
+}
