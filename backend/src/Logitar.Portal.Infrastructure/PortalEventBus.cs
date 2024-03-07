cea550d124cb2e5d0e50e09b98d0057a34046e80
Francis Pion <francispion@hotmail.com>
2024-03-07 00:23:41 -0500

    Fixed actor cache not cleared. (#434)

--- backend/src/Logitar.Portal.Infrastructure/PortalEventBus.cs
+++ backend/src/Logitar.Portal.Infrastructure/PortalEventBus.cs
@@ -1,6 +1,7 @@
 ﻿using Logitar.EventSourcing;
 using Logitar.Identity.Infrastructure;
 using Logitar.Identity.Infrastructure.Handlers;
+using Logitar.Portal.Application.Caching;
 using Logitar.Portal.Application.Logging;
 using MediatR;
 
@@ -8,11 +9,13 @@ namespace Logitar.Portal.Infrastructure;
 
 internal class PortalEventBus : EventBus
 {
+  private readonly ICacheService _cacheService;
   private readonly ILoggingService _loggingService;
 
-  public PortalEventBus(ILoggingService loggingService, IPublisher publisher, IApiKeyEventHandler apiKeyEventHandler, IOneTimePasswordEventHandler oneTimePasswordEventHandler, IRoleEventHandler roleEventHandler, ISessionEventHandler sessionEventHandler, IUserEventHandler userEventHandler)
+  public PortalEventBus(ICacheService cacheService, ILoggingService loggingService, IPublisher publisher, IApiKeyEventHandler apiKeyEventHandler, IOneTimePasswordEventHandler oneTimePasswordEventHandler, IRoleEventHandler roleEventHandler, ISessionEventHandler sessionEventHandler, IUserEventHandler userEventHandler)
     : base(publisher, apiKeyEventHandler, oneTimePasswordEventHandler, roleEventHandler, sessionEventHandler, userEventHandler)
   {
+    _cacheService = cacheService;
     _loggingService = loggingService;
   }
 
@@ -21,5 +24,15 @@ internal class PortalEventBus : EventBus
     _loggingService.Report(@event);
 
     await base.PublishAsync(@event, cancellationToken);
+
+    string? @namespace = @event.GetType().Namespace;
+    switch (@namespace)
+    {
+      case "Logitar.Identity.Domain.ApiKeys.Events":
+      case "Logitar.Identity.Domain.Users.Events":
+        ActorId actorId = new(@event.AggregateId.Value);
+        _cacheService.RemoveActor(actorId);
+        break;
+    }
   }
 }
