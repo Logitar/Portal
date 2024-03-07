9bc50e491fc43d95a7f9c3e79649747b4a488513
Francis Pion <francispion@hotmail.com>
2024-03-06 22:33:43 -0500

    Implemented Bearer authentication. (#429)

--- backend/src/Logitar.Portal/Constants/Schemes.cs
+++ backend/src/Logitar.Portal/Constants/Schemes.cs
@@ -4,13 +4,15 @@ internal static class Schemes
 {
   public const string ApiKey = nameof(ApiKey);
   public const string Basic = nameof(Basic);
+  public const string Bearer = nameof(Bearer);
   public const string Session = nameof(Session);
 
   public static string[] GetEnabled(IConfiguration configuration)
   {
-    List<string> schemes = new(capacity: 3)
+    List<string> schemes = new(capacity: 4)
     {
       ApiKey,
+      Bearer,
       Session
     };
 
