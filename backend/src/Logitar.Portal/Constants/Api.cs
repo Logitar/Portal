7423c98dfbe4f98cd9debb30581727190b4f3832
Francis Pion <francispion@hotmail.com>
2024-03-09 18:51:40 -0500

    Release 4.0.1 (#462)

--- backend/src/Logitar.Portal/Constants/Api.cs
+++ backend/src/Logitar.Portal/Constants/Api.cs
@@ -3,5 +3,5 @@
 internal static class Api
 {
   public const string Title = "Portal API";
-  public static readonly Version Version = new(4, 0, 0);
+  public static readonly Version Version = new(4, 0, 1);
 }
