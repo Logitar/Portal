38f4b3ef9682aee8d46bf9fd16ffcbda05d68588
Francis Pion <francispion@hotmail.com>
2024-03-27 01:46:43 -0400

    Completed integration tests.

---
+++ backend/tests/Logitar.Portal.IntegrationTests/Application/Messages/Settings/TwilioTestSettings.cs
@@ -0,0 +1,22 @@
+﻿using Logitar.Portal.Contracts.Senders;
+
+namespace Logitar.Portal.Application.Messages.Settings;
+
+internal record TwilioTestSettings : ITwilioSettings
+{
+  public string AccountSid { get; set; }
+  public string AuthenticationToken { get; set; }
+
+  public string PhoneNumber { get; set; }
+
+  public TwilioTestSettings() : this(string.Empty, string.Empty, string.Empty)
+  {
+  }
+
+  public TwilioTestSettings(string accountSid, string authenticationToken, string phoneNumber)
+  {
+    AccountSid = accountSid;
+    AuthenticationToken = authenticationToken;
+    PhoneNumber = phoneNumber;
+  }
+}
