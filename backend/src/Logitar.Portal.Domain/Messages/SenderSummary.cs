2ea151d387ea2ae27b82b91e3ef03a00caeb5c19
Francis Pion <francispion@hotmail.com>
2024-03-26 23:12:14 -0400

    Implemented sender domain layer.

--- backend/src/Logitar.Portal.Domain/Messages/SenderSummary.cs
+++ backend/src/Logitar.Portal.Domain/Messages/SenderSummary.cs
@@ -9,23 +9,36 @@ public record SenderSummary
 {
   public SenderId Id { get; }
   public bool IsDefault { get; }
-  public EmailUnit Email { get; }
+  public EmailUnit? Email { get; }
+  public PhoneUnit? Phone { get; }
   public DisplayNameUnit? DisplayName { get; }
   public SenderProvider Provider { get; }
 
-  [JsonConstructor]
   public SenderSummary(SenderId id, bool isDefault, EmailUnit email, DisplayNameUnit? displayName, SenderProvider provider)
+    : this(id, isDefault, email, phone: null, displayName, provider)
+  {
+  }
+  [JsonConstructor]
+  public SenderSummary(SenderId id, bool isDefault, EmailUnit? email, PhoneUnit? phone, DisplayNameUnit? displayName, SenderProvider provider)
   {
     Id = id;
     IsDefault = isDefault;
     Email = email;
+    Phone = phone;
     DisplayName = displayName;
     Provider = provider;
   }
 
-  public SenderSummary(SenderAggregate sender) : this(sender.Id, sender.IsDefault, sender.Email, sender.DisplayName, sender.Provider)
+  public SenderSummary(SenderAggregate sender) : this(sender.Id, sender.IsDefault, sender.Email, sender.Phone, sender.DisplayName, sender.Provider)
   {
   }
 
-  public MailAddress ToMailAddress() => new(Email.Address, DisplayName?.Value);
+  public MailAddress ToMailAddress()
+  {
+    if (Email == null)
+    {
+      throw new InvalidOperationException($"The sender must be an {nameof(SenderType.Sms)} sender in order to be converted to a {nameof(MailAddress)}.");
+    }
+    return new(Email.Address, DisplayName?.Value);
+  }
 }
---
+++ backend/src/Logitar.Portal.Domain/Senders/SenderType.cs
@@ -0,0 +1,7 @@
+﻿namespace Logitar.Portal.Domain.Senders;
+
+public enum SenderType
+{
+  Email = 0,
+  Sms = 1
+}
