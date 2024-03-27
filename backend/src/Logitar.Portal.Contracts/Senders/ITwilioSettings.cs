1ab40dbb1d8abb97c01cd61e99a1cb5bfe6a19d9
Francis Pion <francispion@hotmail.com>
2024-03-26 22:39:12 -0400

    Implemented Twilio contracts.

---
+++ backend/src/Logitar.Portal.Contracts/Senders/ITwilioSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Logitar.Portal.Contracts.Senders;
+
+public interface ITwilioSettings
+{
+  string AccountSid { get; }
+  string AuthenticationToken { get; }
+}
---
+++ backend/src/Logitar.Portal.Contracts/Senders/TwilioSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Logitar.Portal.Contracts.Senders;
+
+public record TwilioSettings : ITwilioSettings
+{
+  public string AccountSid { get; set; }
+  public string AuthenticationToken { get; set; }
+
+  public TwilioSettings() : this(string.Empty, string.Empty)
+  {
+  }
+
+  public TwilioSettings(string accountSid, string authenticationToken)
+  {
+    AccountSid = accountSid;
+    AuthenticationToken = authenticationToken;
+  }
+}
