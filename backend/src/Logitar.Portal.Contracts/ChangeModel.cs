694db311c52b2fab8e7d5f832c6685bdc27b5f18
Francis Pion <francispion@hotmail.com>
2024-12-23 11:27:23 -0500

    contracts

---
+++ backend/src/Logitar.Portal.Contracts/ChangeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Logitar.Portal.Contracts;
+
+public record ChangeModel<T>
+{
+  public T? Value { get; set; }
+
+  public ChangeModel(T? value = default)
+  {
+    Value = value;
+  }
+}
--- backend/src/Logitar.Portal.Contracts/Realms/UpdateRealmPayload.cs
+++ backend/src/Logitar.Portal.Contracts/Realms/UpdateRealmPayload.cs
@@ -1,17 +1,16 @@
-﻿using Logitar.Identity.Contracts;
-using Logitar.Portal.Contracts.Settings;
+﻿using Logitar.Portal.Contracts.Settings;
 
 namespace Logitar.Portal.Contracts.Realms;
 
 public record UpdateRealmPayload
 {
   public string? UniqueSlug { get; set; }
-  public Modification<string>? DisplayName { get; set; }
-  public Modification<string>? Description { get; set; }
+  public ChangeModel<string>? DisplayName { get; set; }
+  public ChangeModel<string>? Description { get; set; }
 
-  public Modification<string>? DefaultLocale { get; set; }
+  public ChangeModel<string>? DefaultLocale { get; set; }
   public string? Secret { get; set; }
-  public Modification<string>? Url { get; set; }
+  public ChangeModel<string>? Url { get; set; }
 
   public UniqueNameSettings? UniqueNameSettings { get; set; }
   public PasswordSettings? PasswordSettings { get; set; }
