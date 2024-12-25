df80de07f1a10fe69c8b1609d066a490aeec0ef0
Francis Pion <francispion@hotmail.com>
2024-12-25 10:11:15 -0500

    Refactored sender & message management.

--- backend/src/Logitar.Portal.Infrastructure/Messages/MessageExtensions.cs
+++ backend/src/Logitar.Portal.Infrastructure/Messages/MessageExtensions.cs
@@ -37,4 +37,52 @@ internal static class MessageExtensions
 
     return new SmsMessage(from: aggregate.Sender.ToE164PhoneNumber(), to: recipient.PhoneNumber, body: aggregate.Body.Text);
   }
+
+  public static MailMessage ToMailMessage(this MessageAggregate message)
+  {
+    MailMessage mailMessage = new()
+    {
+      From = message.Sender.ToMailAddress(),
+      Subject = message.Subject.Value,
+      Body = message.Body.Text,
+      IsBodyHtml = message.Body.Type == MediaTypeNames.Text.Html
+    };
+
+    foreach (RecipientUnit recipient in message.Recipients)
+    {
+      MailAddress address = recipient.ToMailAddress();
+      switch (recipient.Type)
+      {
+        case RecipientType.Bcc:
+          mailMessage.Bcc.Add(address);
+          break;
+        case RecipientType.CC:
+          mailMessage.CC.Add(address);
+          break;
+        case RecipientType.To:
+          mailMessage.To.Add(address);
+          break;
+      }
+    }
+
+    return mailMessage;
+  }
+
+  public static MailAddress ToMailAddress(this SenderSummary sender)
+  {
+    if (sender.Email == null)
+    {
+      throw new ArgumentException($"The sender must be an {nameof(SenderType.Email)} sender in order to be converted into a {nameof(MailAddress)}.", nameof(sender));
+    }
+    return new(sender.Email.Address, sender.DisplayName?.Value);
+  }
+
+  public static MailAddress ToMailAddress(this RecipientUnit recipient)
+  {
+    if (recipient.Address == null)
+    {
+      throw new ArgumentException($"A recipient requires an email address in order to be converted into a {nameof(MailAddress)}.", nameof(recipient));
+    }
+    return new(recipient.Address, recipient.DisplayName);
+  }
 }
