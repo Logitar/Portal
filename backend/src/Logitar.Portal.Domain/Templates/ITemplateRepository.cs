49721deaf5e3c607120539672548cf7a3e904bac
Francis Pion <francispion@hotmail.com>
2024-12-26 15:41:26 -0500

    TemplateRepository

--- backend/src/Logitar.Portal.Domain/Templates/ITemplateRepository.cs
+++ backend/src/Logitar.Portal.Domain/Templates/ITemplateRepository.cs
@@ -6,8 +6,8 @@ public interface ITemplateRepository
 {
   Task<Template?> LoadAsync(Guid id, CancellationToken cancellationToken = default);
   Task<Template?> LoadAsync(TemplateId id, long? version, CancellationToken cancellationToken = default);
-  Task<IEnumerable<Template>> LoadAsync(CancellationToken cancellationToken = default);
-  Task<IEnumerable<Template>> LoadAsync(TenantId? tenantId, CancellationToken cancellationToken = default);
+  Task<IReadOnlyCollection<Template>> LoadAsync(CancellationToken cancellationToken = default);
+  Task<IReadOnlyCollection<Template>> LoadAsync(TenantId? tenantId, CancellationToken cancellationToken = default);
   Task<Template?> LoadAsync(TenantId? tenantId, UniqueKey uniqueKey, CancellationToken cancellationToken = default);
 
   Task SaveAsync(Template template, CancellationToken cancellationToken = default);
